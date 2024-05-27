6c30c6d8f954e32dc92cf9c6c669de5f163afdc9
GudulePoilu <antoine.calma30@gmail.com>
2024-05-27 09:00:29 +0200

    maquette et route

--- portfolio/app/api/works/route.ts
+++ portfolio/app/api/works/route.ts
@@ -1,18 +1,18 @@
 export async function GET() {
-  return new Response({"methode": 'GET'})
+  return new Response(JSON.stringify({"methode": 'GET'}))
 }
 
 export async function POST(req: Request) {
-  const body = req.json().then(body => body)
-  return new Response({"methode": 'POST', body: body})
+  const body = await req.json().then(body => body)
+  return new Response(JSON.stringify({"methode": 'POST', body: body}))
 }
 
 export async function PUT(req:Request) {
-  const body = req.json().then(body => body)
-  return new Response({"methode": 'PUT', body: body})
+  const body = await req.json().then(body => body)
+  return new Response(JSON.stringify({"methode": 'PUT', body: body}))
 }
 
 export async function DELETE(req: Request) {
-  const workId = req.nextUrl.searchParams//.get("workId")
-  return new Response("DELETE" + " " + workId)
+  const workId = req.nextUrl.searchParams.get("workId")
+  return new Response(JSON.stringify({"methode": "DELETE", "workId": workId}))
 }
