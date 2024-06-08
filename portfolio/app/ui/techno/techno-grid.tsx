a213c1a8092f77fe7b428eed12c919355e227952
Amlac Eniotna <antoine.calma30@gmail.com>
2024-06-08 14:29:52 +0200

    shuffle techno list

--- portfolio/app/ui/techno/techno-grid.tsx
+++ portfolio/app/ui/techno/techno-grid.tsx
@@ -1,20 +1,35 @@
 import style from "@/app/ui/techno/animation-techno.module.css";
 import { Techno } from "@/app/ui/works/techno-avatar";
-const technos = ["redux", "react", "nextjs", "sass", "tailwind", "typescript"];
-const repeat = [0, 1, 2];
+const technoslist = [
+  "redux",
+  "react",
+  "nextjs",
+  "sass",
+  "tailwind",
+  "typescript",
+];
+const repeat = [0, 1, 2, 3, 4];
 
 export function GridTechno() {
+  function shuffle(array: string[]) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+  let technos = technoslist;
   return (
     <div
       className={`relative flex h-[70vh] w-full flex-col items-center justify-evenly overflow-hidden rounded-3xl bg-wmauve-3 xl:h-[650px] xl:p-8 dark:bg-mauve-3`}
     >
       {repeat.map((n) => {
+        shuffle(technos);
         return (
           <div
             key={n}
             className={`${n & 1 ? style.animation : style.animationb} relative flex flex-nowrap justify-between gap-4`}
             style={{
-              animationDuration: `${Math.floor(Math.random() * 20) + 10}s`,
+              animationDuration: `${Math.floor(Math.random() * 30) + 10}s`,
               animationDelay: `${Math.floor(Math.random() * 30) - 31}s`,
             }}
           >
